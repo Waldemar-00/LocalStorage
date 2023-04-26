fa271fc07ac5f08733e51f06b2485dacb05ff1f5
uladzimir <vova108vova@gmail.com>
2023-04-26 21:32:04 +0300

    scale, color changing on the page and reset

--- js/customizator.js
+++ js/customizator.js
@@ -1,27 +1,82 @@
 export default class Customizator {
   constructor () {
     this.buttonsBlock = document.createElement('div')
-    this.buttonsBlock.addEventListener('click', (e) => { this.scale(e) })
     this.colorPicker = document.createElement('input')
-    this.colorPicker.addEventListener('input', (e) => { this.color(e) })
+    this.cross = document.createElement('div')
+    this.index = localStorage.getItem('index') || 1
+    this.colorFromStorage = localStorage.getItem('color') || '#FFFFFF'
+    this.buttonsBlock.addEventListener('click', (e) => this.scale(e))
+    this.colorPicker.addEventListener('input', (e) => this.color(e))
+    this.cross.addEventListener('click', () => this.reset())
+  }
+
+  createElementStyle () {
+    const style = document.createElement('style')
+    style.innerHTML = `
+      .panel {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        position: fixed;
+        top: 10px;
+        right: 0;
+        border: 1px solid rgba(0,0,0, .2);
+        box-shadow: 0 0 20px rgba(0,0,0, .5);
+        width: 300px;
+        height: 60px;
+        background-color: #fff;
+      }
+      .scale {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100px;
+        height: 40px;
+      }
+      .scale_btn {
+        display: block;
+        width: 40px;
+        height: 40px;
+        border: 1px solid rgba(0,0,0, .2);
+        border-radius: 4px;
+        font-size: 18px;
+      }
+      .color {
+        width: 40px;
+        height: 40px;
+      }
+      .cross {
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        line-height: 40px;
+        font-size: 40xp;
+        font-weight: 600;
+        cursor: pointer;
+        border-radius: 4px;
+      }
+      .cross:hover {
+        background-color: #F0F0F0;
+      }
+    `
+    document.querySelector('head').append(style)
   }
 
   scale (e) {
-    let index
     const body = document.querySelector('body')
-    if (e.target.value) {
-      index = +e.target.value.replace(/^\.\D/g, '')
+    if (e) {
+      this.index = +e.target.value.replace(/^\.\D/g, '')
     }
-    function recursively (elem) {
+    const recursively = (elem) => {
       elem.childNodes.forEach(node => {
         if (node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, '')) {
           if (!node.parentNode.getAttribute('data-fs')) {
             let font = window.getComputedStyle(node.parentNode, null).fontSize
             font = font.replace(/\D/g, '')
             node.parentNode.setAttribute('data-fs', font)
-            node.parentNode.style.fontSize = font * index + 'px'
+            node.parentNode.style.fontSize = font * this.index + 'px'
           } else {
-            node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fs') * index + 'px'
+            node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fs') * this.index + 'px'
           }
         } else {
           recursively(node)
@@ -29,13 +84,31 @@ export default class Customizator {
       })
     }
     recursively(body)
+    localStorage.setItem('index', this.index)
   }
 
   color (e) {
     document.querySelector('body').style.backgroundColor = e.target.value
+    localStorage.setItem('color', e.target.value)
+  }
+
+  setColor () {
+    document.querySelector('body').style.backgroundColor = this.colorFromStorage
+    this.colorPicker.value = this.colorFromStorage
+  }
+
+  reset () {
+    localStorage.clear()
+    this.index = 1
+    this.colorFromStorage = '#FFFFFF'
+    this.setColor()
+    this.scale()
   }
 
   render () {
+    this.createElementStyle()
+    this.setColor()
+    this.scale()
     const scaleButtonSmall = document.createElement('input')
     const scaleButtonMedium = document.createElement('input')
     const topPanel = document.createElement('div')
@@ -51,9 +124,11 @@ export default class Customizator {
     this.colorPicker.setAttribute('value', '#FFB35C')
     this.colorPicker.style.border = '1px solid rgba(0,0,0, .2)'
     this.colorPicker.style.borderRadius = '4px'
+    this.cross.innerHTML = 'reset'
+    this.cross.classList.add('cross')
     this.buttonsBlock.append(scaleButtonSmall, scaleButtonMedium)
     topPanel.classList.add('panel')
-    topPanel.append(this.buttonsBlock, this.colorPicker)
+    topPanel.append(this.buttonsBlock, this.colorPicker, this.cross)
     document.querySelector('body').append(topPanel)
     console.log(topPanel)
   }
